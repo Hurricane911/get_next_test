5f4172c5fe777f697ef5afb433e7941d9fe3ae04
Joyson Yim <joyim@u91z03s04.42kl.edu.my>
2024-07-26 19:20:23 +0800

    update

--- get_next/get_next_line.h
+++ get_next/get_next_line.h
@@ -1,15 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: joyim <joyim@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/26 17:07:21 by joyim             #+#    #+#             */
+/*   Updated: 2024/07/26 17:21:53 by joyim            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef GET_NEXT_LINE_H
-#define GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
-#include <stdlib.h>
-#include <unistd.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
 
-static char	*whole_line(int fd, char *buf, char *backup);
-static char second_line(char *string);
-char	*ft_strjoin(char const *s1, char const *s2);
-char	*ft_strrchr(const char *s, int c);
-char	*ft_substr(char const *s, unsigned int start, size_t len);
 size_t	ft_strlen(const char *s);
-char *get_next_line(int fd);
+char	*ft_strdup(const char *s1);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+void	*ft_memcpy(void *dst, const void *src, size_t n);
+char	*get_next_line(int fd);
 
 #endif
--- get_next/get_next_line_utils.c
+++ get_next/get_next_line_utils.c
@@ -1,4 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: joyim <joyim@student.42kl.edu.my>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/26 17:07:46 by joyim             #+#    #+#             */
+/*   Updated: 2024/07/26 17:07:49 by joyim            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	length;
+
+	length = 0;
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+	return (length);
+}
+
+char	*ft_strdup(const char *s1)
+{
+	char	*str;
+	size_t	i;
+
+	str = (char *)malloc(sizeof(char) * (ft_strlen(s1) + 1));
+	if (str == NULL)
+		return (NULL);
+	i = 0;
+	while (s1[i])
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	str[i] = 0;
+	return (str);
+}
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
@@ -28,26 +70,23 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	return (ptr);
 }
 
-char	*ft_strrchr(const char *s, int c)
+void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
-	char	*last;
-	size_t	i;
+	unsigned char	*d;
+	unsigned char	*s;
 
-	i = 0;
-	last = NULL;
-	while (s[i] != '\0')
+	d = (unsigned char *)dst;
+	s = (unsigned char *)src;
+	if (n == 0)
+		return (dst);
+	if (dst == NULL && src == NULL)
+		return (NULL);
+	while (n > 0)
 	{
-		if (s[i] == (char)c)
-		{
-			last = ((char *)&s[i]);
-		}
-		i++;
+		*d++ = *s++;
+		n--;
 	}
-	if ((char)c == '\0')
-	{
-		return ((char *)&s[i]);
-	}
-	return (last);
+	return (dst);
 }
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
@@ -72,14 +111,22 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	return (substr);
 }
 
-size_t	ft_strlen(const char *s)
-{
-	size_t	length;
+// static char	*ft_strchr(const char *s, int c)
+// {
+// 	size_t	i;
 
-	length = 0;
-	while (s[length] != '\0')
-	{
-		length++;
-	}
-	return (length);
-}
+// 	i = 0;
+// 	while (s[i] != '\0')
+// 	{
+// 		if (s[i] == (unsigned char)c)
+// 		{
+// 			return ((char *)&s[i]);
+// 		}
+// 		i++;
+// 	}
+// 	if (c == '\0')
+// 	{
+// 		return ((char *)&s[i]);
+// 	}
+// 	return (NULL);
+// }
